582a4e5d91ec10cb832032334829234f6ae23005
SeppPenner <HansM2013@gmx.de>
2020-09-27 18:56:59 +0200

    Added option to push packages.

---
+++ PushPackage.bat
@@ -0,0 +1,3 @@
+dotnet nuget push "src\Serilog.Sinks.AmazonS3\bin\Release\HaemmerElectronics.SeppPenner.Serilog.Sinks.AmazonS3.*.nupkg" -s "github" --skip-duplicate
+dotnet nuget push "src\Serilog.Sinks.AmazonS3\bin\Release\HaemmerElectronics.SeppPenner.Serilog.Sinks.AmazonS3.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
+PAUSE
