42e3dadc25ef7ca1b4113ee7e4b2d409d44c69ea
SeppPenner <HansM2013@gmx.de>
2020-11-09 20:14:04 +0100

    Adjusted tests.

--- src/Serilog.Sinks.AmazonS3.Tests/AmazonS3BasicTests.cs
+++ src/Serilog.Sinks.AmazonS3.Tests/AmazonS3BasicTests.cs
@@ -15,24 +15,33 @@ namespace Serilog.Sinks.AmazonS3.Tests
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-    using Serilog.Core;
     using Serilog.Events;
     using Serilog.Formatting.Compact;
 
-    /// <summary>   This class is used for some basic test regarding the Amazon S3 sink. </summary>
+    /// <summary>
+    /// This class is used for some basic test regarding the Amazon S3 sink.
+    /// </summary>
     [TestClass]
     public class AmazonS3BasicTests
     {
-        /// <summary>   The Amazon S3 access key id. </summary>
-        private const string AwsAccessKeyId = "yourAccessKeyId";
+        /// <summary>
+        /// The Amazon S3 access key id.
+        /// </summary>
+        private const string AwsAccessKeyId = "Dummy";
 
-        /// <summary>   The Amazon S3 secret access key. </summary>
-        private const string AwsSecretAccessKey = "yourSecretAccessKey";
+        /// <summary>
+        /// The Amazon S3 secret access key.
+        /// </summary>
+        private const string AwsSecretAccessKey = "Dummy";
 
-        /// <summary>   The Amazon S3 bucket name. </summary>
-        private const string BucketName = "your-test-bucket";
+        /// <summary>
+        /// The Amazon S3 bucket name.
+        /// </summary>
+        private const string BucketName = "dummy";
 
-        /// <summary>   This method is used to test a basic file upload to Amazon S3. </summary>
+        /// <summary>
+        /// This method is used to test a basic file upload to Amazon S3 with no credentials (IAM).
+        /// </summary>
         [TestMethod]
         public void BasicFileUploadAuthorizedTest()
         {
@@ -40,29 +49,18 @@ namespace Serilog.Sinks.AmazonS3.Tests
                 "log.txt",
                 BucketName,
                 RegionEndpoint.EUWest2,
-                fileSizeLimitBytes: 200,
+                LogEventLevel.Verbose,
+                outputTemplate: null,
+                formatProvider: null,
+                levelSwitch: null,
                 rollingInterval: RollingInterval.Minute,
-                failureCallback: e => Console.WriteLine($"Sink error: {e.Message}")).CreateLogger();
-
-            for (var x = 0; x < 200; x++)
-            {
-                var ex = new Exception("Test");
-                logger.Error(ex.ToString());
-            }
-        }
-
-        /// <summary>   This method is used to test a basic file upload to Amazon S3. </summary>
-        [TestMethod]
-        public void BasicFileUploadTest()
-        {
-            var logger = new LoggerConfiguration().WriteTo.AmazonS3(
-                "log.txt",
-                BucketName,
-                RegionEndpoint.EUWest2,
-                AwsAccessKeyId,
-                AwsSecretAccessKey,
-                fileSizeLimitBytes: 200,
-                rollingInterval: RollingInterval.Minute).CreateLogger();
+                encoding: null,
+                failureCallback: e => Console.WriteLine($"Sink error: {e.Message}, {e.StackTrace}"),
+                bucketPath: null,
+                batchSizeLimit: null,
+                batchingPeriod: null,
+                eagerlyEmitFirstEvent: null,
+                queueSizeLimit: null).CreateLogger();
 
             for (var x = 0; x < 200; x++)
             {
@@ -72,29 +70,31 @@ namespace Serilog.Sinks.AmazonS3.Tests
         }
 
         /// <summary>
-        ///     This method is used to test find the null reference exception of issue 8.
+        /// This method is used to test a basic file upload to Amazon S3.
         /// </summary>
+        // ReSharper disable ArgumentsStyleLiteral
+        // ReSharper disable ArgumentsStyleNamedExpression
         [TestMethod]
-        public void ExtendedFileUploadTest()
+        public void BasicFileUploadTest()
         {
-            var levelSwitch = new LoggingLevelSwitch { MinimumLevel = LogEventLevel.Information };
-
-            var logger = new LoggerConfiguration()
-                .WriteTo.AmazonS3(
-                    "log.txt",
-                    BucketName,
-                    RegionEndpoint.EUWest2,
-                    AwsAccessKeyId,
-                    AwsSecretAccessKey,
-                    restrictedToMinimumLevel: LogEventLevel.Verbose,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
-                    fileSizeLimitBytes: 200,
-                    levelSwitch: levelSwitch,
-                    buffered: true,
-                    rollingInterval: RollingInterval.Minute,
-                    retainedFileCountLimit: 10,
-                    autoUploadEvents: true)
-                .CreateLogger();
+            var logger = new LoggerConfiguration().WriteTo.AmazonS3(
+                "log.txt",
+                BucketName,
+                RegionEndpoint.EUWest2,
+                AwsAccessKeyId,
+                AwsSecretAccessKey,
+                LogEventLevel.Verbose,
+                outputTemplate: null,
+                formatProvider: null,
+                levelSwitch: null,
+                rollingInterval: RollingInterval.Minute,
+                encoding: null,
+                failureCallback: e => Console.WriteLine($"Sink error: {e.Message}, {e.StackTrace}"),
+                bucketPath: null,
+                batchSizeLimit: null,
+                batchingPeriod: null,
+                eagerlyEmitFirstEvent: null,
+                queueSizeLimit: null).CreateLogger();
 
             for (var x = 0; x < 200; x++)
             {
@@ -109,24 +109,23 @@ namespace Serilog.Sinks.AmazonS3.Tests
         [TestMethod]
         public void JsonFileUploadTest()
         {
-            var levelSwitch = new LoggingLevelSwitch { MinimumLevel = LogEventLevel.Information };
-
-            var logger = new LoggerConfiguration()
-                .WriteTo.AmazonS3(
-                    new CompactJsonFormatter(),
-                    "log.json",
-                    BucketName,
-                    RegionEndpoint.EUWest2,
-                    AwsAccessKeyId,
-                    AwsSecretAccessKey,
-                    restrictedToMinimumLevel: LogEventLevel.Verbose,
-                    fileSizeLimitBytes: 200,
-                    levelSwitch: levelSwitch,
-                    buffered: true,
-                    rollingInterval: RollingInterval.Minute,
-                    retainedFileCountLimit: 10,
-                    autoUploadEvents: true)
-                .CreateLogger();
+            var logger = new LoggerConfiguration().WriteTo.AmazonS3(
+                "log.txt",
+                BucketName,
+                RegionEndpoint.EUWest2,
+                AwsAccessKeyId,
+                AwsSecretAccessKey,
+                LogEventLevel.Verbose,
+                formatter: new CompactJsonFormatter(),
+                levelSwitch: null,
+                rollingInterval: RollingInterval.Minute,
+                encoding: null,
+                failureCallback: e => Console.WriteLine($"Sink error: {e.Message}, {e.StackTrace}"),
+                bucketPath: null,
+                batchSizeLimit: null,
+                batchingPeriod: null,
+                eagerlyEmitFirstEvent: null,
+                queueSizeLimit: null).CreateLogger();
 
             for (var x = 0; x < 200; x++)
             {
