3016ff6744b4b4f6fb47a3764d08cd9398f21f64
SeppPenner <HansM2013@gmx.de>
2021-04-07 20:47:03 +0200

    Fixed spelling issue.

--- src/Serilog.Sinks.AmazonS3/Sinks/AmazonS3/AmazonS3Sink.cs
+++ src/Serilog.Sinks.AmazonS3/Sinks/AmazonS3/AmazonS3Sink.cs
@@ -61,7 +61,7 @@ namespace Serilog.Sinks.AmazonS3
             this.amazonS3Options.Encoding = amazonS3Options.Encoding;
             this.amazonS3Options.FailureCallback = amazonS3Options.FailureCallback;
             this.amazonS3Options.BucketPath = amazonS3Options.BucketPath;
-            this.amazonS3Options.ServiceURL = amazonS3Options.ServiceURL;
+            this.amazonS3Options.ServiceUrl = amazonS3Options.ServiceUrl;
         }
 
         /// <summary>Emit a batch of log events, running asynchronously.</summary>
