d2be5ef8820d4e783392fd958e6038303d7e8dc9
SeppPenner <HansM2013@gmx.de>
2022-02-17 19:23:58 +0100

    Smaller changes.

--- src/Serilog.Sinks.AmazonS3.Tests/GlobalUsings.cs
+++ src/Serilog.Sinks.AmazonS3.Tests/GlobalUsings.cs
@@ -1,8 +1,8 @@
-#pragma warning disable IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
 global using Amazon;
 
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 global using Serilog.Events;
 global using Serilog.Formatting.Compact;
-#pragma warning restore IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
